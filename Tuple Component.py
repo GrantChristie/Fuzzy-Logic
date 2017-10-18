48b0091683b8fd8cc260864b815619a77cf567f3
Grant Christie <grantchristie0@gmail.com>
2017-10-18 18:16:57 +0100

    refactor component 2 to be modular

--- Tuple Component.py
+++ Tuple Component.py
@@ -1,19 +1,27 @@
 #4 tuple fuzzy representation - lecture slide 20
 
-a = int(input("Enter a value: "))
-b = int(input("Enter b value: "))
-alpha = int(input("Enter alpha value: "))
-beta = int(input("Enter beta value: "))
+def main():
+    
 
-x = int(input("Enter test value: "))
+    a = int(input("Enter a value: "))
+    b = int(input("Enter b value: "))
+    alpha = int(input("Enter alpha value: "))
+    beta = int(input("Enter beta value: "))
 
-if x < a - alpha:
-    print(0)
-elif x in range(a - alpha, a):
-        print((x - a + alpha )/alpha)
-elif x in range(a, b):
-    print(1)
-elif x in range(b, b + beta):
-    print((b + beta - x)/beta)
-elif x > b + beta:
-    print(0)
+    x = int(input("Enter test value: "))
+    membership(a,b,alpha,beta,x)
+    
+def membership(a,b,alpha,beta,x):
+    if x < a - alpha:
+        print(0)
+    elif x in range(a - alpha, a):
+            print((x - a + alpha )/alpha)
+    elif x in range(a, b):
+        print(1)
+    elif x in range(b, b + beta):
+        print((b + beta - x)/beta)
+    elif x > b + beta:
+        print(0)
+
+if __name__ == '__main__':
+    main()
