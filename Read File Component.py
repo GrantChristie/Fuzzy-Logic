ee209d25c39abacf1d934c85e3b9c6596f76d10b
Grant Christie <grantchristie0@gmail.com>
2017-10-18 18:13:17 +0100

    refactor component 1 to be modular

--- Read File Component.py
+++ Read File Component.py
@@ -1,19 +1,31 @@
 import re
 
-filename = input("Enter file to be processed: ")
-print(filename)
-f = open(filename, "r")
-file_contents = (re.split(r'\n\n',f.read()))#split text file by using every double whitespace
-rule_base =  file_contents[0]
-rules = file_contents[1]
-fuzzy_sets = []
-measurements = file_contents[len(file_contents)-1] #last 'paragraph' will be the measurements
+def main():
+    filename = input("Enter file to be processed: ")
+    file = read_file(filename)    
 
-for i in range(2,len(file_contents)-1,2): #Locate and group fuzzy set values
-    fuzzy_sets.append(file_contents[i+1].split("\n"))
-f.close()
+    print(file)
 
-print (rule_base)
-print (rules)
-print (fuzzy_sets)
-print (measurements)
+def read_file(filename):
+    info = {}
+    f = open(filename, "r")
+    file_contents = (re.split(r'\n\n',f.read()))#split text file by using every double whitespace
+
+    rule_base =  file_contents[0]
+    rules = file_contents[1]
+    fuzzy_sets = []
+    measurements = file_contents[len(file_contents)-1] #last 'paragraph' will be the measurements
+
+    for i in range(2,len(file_contents)-1,2): #Locate and group fuzzy set values
+        fuzzy_sets.append(file_contents[i+1].split("\n"))
+    f.close()
+
+    info['Rule_Base'] = rule_base
+    info['Rules'] = rules.split("\n")
+    info['Fuzzy_Sets'] = fuzzy_sets;
+    info['RealWorld'] = measurements.split("\n");
+
+    return info
+
+if __name__ == '__main__':
+    main()
