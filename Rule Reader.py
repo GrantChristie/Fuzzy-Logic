6b5b71c115d7a6191fa7a99b17f66fc1bfcbba95
GrantChristie <grantchristie0@gmail.com>
2017-10-18 12:35:20 +0100

    basic rule reader complete

--- Rule Reader.py
+++ Rule Reader.py
@@ -2,14 +2,21 @@ import re
 
 rule = input("Enter rule: ")
 
-x = re.search("Rule|rule (?P<RuleID>[0-9]*) if the (?P<Variable>[0-1A-Za-z_]+) is (?P<Value>[0-1A-Za-z_]+)", rule)
+x = re.search("(Rule|rule) (?P<RuleID>[0-9]*) if the (?P<Variable1>[0-1A-Za-z_]+) is (?P<Value1>[0-1A-Za-z_]+) (?P<Operator>and|or) the (?P<Variable2>[0-1A-Za-z_]+) is (?P<Value2>[0-1A-Za-z_]+) then the (?P<Output>[0-1A-Za-z]+) will be (?P<Value>[0-1A-Za-z]+)" , rule)
 
 if x is None:
     print("Invalid rule")
 else:
     ID = x.group("RuleID")
     print(ID)#remove when done
-    variable = x.group("Variable")
-    print(variable)#remove when done
+    variables = [x.group("Variable1"), x.group("Variable2")]
+    print(variables)
+    values = [x.group("Value1"),x.group("Value2")]
+    print (values)
+    operator = x.group("Operator")
+    print(operator)
+    output = x.group("Output")
+    print(output)
     value = x.group("Value")
-    print(value)#remove when done
+    print(value)
+    
